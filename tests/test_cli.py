30403c0d3475dbf2ca348d0c464fd53e38deb0da
pawel08 <kazalapawel08@gmail.com>
2026-06-08 19:45:13 +0200

    Naprawa błędu

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,5 @@
 """Unit tests for the CLI module."""
-
+from cli import main
 import os
 import sys
 from unittest.mock import patch
@@ -7,7 +7,7 @@ from unittest.mock import patch
 # Ensure the root directory is in the path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from cli import main
+
 
 
 def test_cli_addition():
