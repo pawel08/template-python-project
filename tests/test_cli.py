f74495d58865e3865b321edf6705a0b85ac27ecf
pawel08 <kazalapawel08@gmail.com>
2026-06-08 19:48:49 +0200

    próba

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,8 +2,8 @@
 import os
 import sys
 from unittest.mock import patch
-from cli import main
 
+from cli import main
 
 # Ensure the root directory is in the path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
